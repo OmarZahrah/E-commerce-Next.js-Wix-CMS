b2f9710117d02f28d45f40789a5abe77377ff078
OmarZahrah <omar.zahrah01@gmail.com>
2025-03-07 15:50:18 +0200

    Add transition to nav menu on small screens

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -17,17 +17,34 @@ const Menu = () => {
         className="cursor-pointer"
         onClick={() => setOpen((prev) => !prev)}
       />
-      {open && (
-        <nav className="absolute bg-black text-white left-0 top-20 w-full h-[calc(100vh-5rem)] flex flex-col items-center justify-center gap-8 text-xl z-10 bg-opacity-90">
-          <Link href="/"> Home </Link>
-          <Link href="/"> Shop </Link>
-          <Link href="/"> Deals </Link>
-          <Link href="/"> About </Link>
-          <Link href="/"> Contact </Link>
-          <Link href="/"> Logout </Link>
-          <Link href="/"> Cart </Link>
-        </nav>
-      )}
+      {/* {open && ( */}
+      <nav
+        className={`absolute bg-black text-white left-0 top-20 w-full h-[calc(100vh-5rem)] flex flex-col items-center justify-center gap-8 text-xl z-10 bg-opacity-90 transition-all ease-in-out duration-1000 
+          ${open ? "translate-x-0" : "translate-x-[100%]"}`}
+      >
+        <Link href="/" className="hover:text-brandPink">
+          Home
+        </Link>
+        <Link href="/" className="hover:text-brandPink">
+          Shop
+        </Link>
+        <Link href="/" className="hover:text-brandPink">
+          Deals
+        </Link>
+        <Link href="/" className="hover:text-brandPink">
+          About
+        </Link>
+        <Link href="/" className="hover:text-brandPink">
+          Contact
+        </Link>
+        <Link href="/" className="hover:text-brandPink">
+          Logout
+        </Link>
+        <Link href="/" className="hover:text-brandPink">
+          Cart
+        </Link>
+      </nav>
+      {/* )} */}
     </div>
   );
 };
