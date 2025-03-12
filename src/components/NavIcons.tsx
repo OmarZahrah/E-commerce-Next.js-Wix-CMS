d4295003cfdd16367bf8925fb8edfcbf2b5376df
OmarZahrah <omar.zahrah01@gmail.com>
2025-03-12 11:24:27 +0200

    create profile page

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -44,7 +44,7 @@ const NavIcons = () => {
       />
       {isProfileOpen && (
         <div className="absolute p-4 rounded-md top-12 left-0 text-sm bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
-          <Link href="/">Profile</Link>
+          <Link href="/profile">Profile</Link>
           <span className="mt-2 cursor-pointer block" onClick={handleLogout}>
             {isLoading ? "Logging Out..." : "Logout"}
           </span>
