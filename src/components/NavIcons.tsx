a48a2a010f3392e37083dac5da08b4321acbdf05
OmarZahrah <omar.zahrah01@gmail.com>
2025-03-16 08:33:30 +0200

    fix error in profile page

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -42,7 +42,7 @@ const NavIcons = () => {
         className="cursor-pointer"
         onClick={handleProfileClick}
       />
-      {isProfileOpen && (
+      {isProfileOpen && isLoggedIn && (
         <div className="absolute p-4 rounded-md top-12 left-0 text-sm bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
           <Link href="/profile">Profile</Link>
           <span className="mt-2 cursor-pointer block" onClick={handleLogout}>
